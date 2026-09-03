23ab2157c2019720328826a13ddeb07bc8dbba20
Wade <show7587@gmail.com>
2026-09-03 11:35:07 +0800

    修正候選人名字：林政宏 → 林成宏 (frontend/src/layouts/MainLayout.vue)

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar density="compact" color="primary">
-    <v-app-bar-title>萬巒鄉鄉民代表候選人 林政宏</v-app-bar-title>
+    <v-app-bar-title>萬巒鄉鄉民代表候選人 林成宏</v-app-bar-title>
   </v-app-bar>
   <router-view />
 </template>
